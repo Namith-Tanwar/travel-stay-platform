51a3f5ee80037fc0830824850c89a8a53391b87b
Namith-Tanwar <tanwarnamith@gmail.com>
2025-12-26 19:55:25 +0530

    Move backend to root and clean project structure

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+
+
+const MONGO_url = 'mongodb://127.0.0.1:27017/travelstay';
+
+main().then(()=>{
+    console.log("connected");
+})
+.catch(err =>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(MONGO_url);
+    console.log("db connected");
+}
+
+app.get("/" , (req,res)=>{
+    res.send("working")
+})
+
+app.get("/listings" , async(req,res)=>{
+    const alllistings = await  Listing.find({});
+})
+
+// app.get("/testlisting" , async(req , res)=>{
+//     let samplelisting = new Listing({
+//         title: "tokyo tower",
+//         description : "beautifult view",
+//         price : 200,
+//         location : "tokyo" ,
+//         country : "Japan", 
+//     })
+//     await samplelisting.save().then(()=>{
+//         console.log("saves ")
+//         res.send("saved")
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//         res.send("error connecting");
+//     })
+
+// })
+
+
+
+
+app.listen(8080 ,()=>{
+    console.log("server is listning")
+})
