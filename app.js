549bcb8664894f14ce84d9143bc2880e303bd541
Namith-Tanwar <tanwarnamith@gmail.com>
2025-12-27 17:33:08 +0530

    Add full CRUD routes for listings

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodoverride = require("method-override");
 
 
 const MONGO_url = 'mongodb://127.0.0.1:27017/travelstay';
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname , "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
 
 main().then(()=>{
     console.log("connected");
@@ -23,9 +29,53 @@ app.get("/" , (req,res)=>{
     res.send("working")
 })
 
+//index route 
 app.get("/listings" , async(req,res)=>{
     const alllistings = await  Listing.find({});
+    res.render("./listings/index.ejs" , {alllistings} )
 })
+
+//new or add listing route
+app.get("/listings/new" , (req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//show route
+app.get("/listings/:id" , async(req , res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs" , {listing});
+})
+
+//create route
+app.post("/listings" , async(req , res) =>{
+    const newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings")
+})
+
+//edit route
+app.get("/listings/:id/edit" , async(req , res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" , {listing});
+})
+
+//update route
+app.put("/listings/:id" , async(req , res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id , {...req.body.listing});
+    res.redirect(`/listings/${id}`); 
+})
+
+//delete route
+app.delete("/listings/:id" , async(req , res)=>{
+    let {id} = req.params;
+    const deletedlisting = await Listing.findByIdAndDelete(id);
+    console.log(deletedlisting);
+    res.redirect("/listings")
+})
+
 
 // app.get("/testlisting" , async(req , res)=>{
 //     let samplelisting = new Listing({
